39525fbdee7ce5192ab961323c4ecc5cee8aee44
Machinocheese <zbill15@yahoo.com>
2017-12-26 20:07:26 -0500

    Added line tracer

--- View.ts
+++ View.ts
@@ -7,23 +7,66 @@ var ctx: CanvasRenderingContext2D;
 var bctx: CanvasRenderingContext2D;
 var sctx: CanvasRenderingContext2D;
 var tree: Tree;
+var speed: number = 1/10; //1 / 10 is WAY too fast...yet I'm going to use it as testing speed.
 
-function drawCircle(x: number, y: number, color: string, radius: number = 5){
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.arc(x,y,radius,0,2*Math.PI);
-    ctx.stroke();
+class Circle{
+    originalX: number;
+    originalY: number;
+    x: number;
+    y: number;
+    radius: number;
+    color: string;
+    constructor(x: number, y: number, color: string, radius: number = 5){
+        this.x = x;
+        this.y = y;
+        this.originalX = x;
+        this.originalY = y;
+        this.color = color;
+        this.radius = radius;
+    }
+    draw(){
+        ctx.save();
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.restore();
+    }
+    update(x: number, y: number){
+        this.x = this.x + x;
+        this.y = this.y + y;
+        this.drawLine();
+    }
+    /**
+     * @description draws a line from the "original" circle position to the border of the new circle position.
+     */
+    drawLine(){
+        ctx.beginPath();
+        ctx.moveTo(this.originalX,this.originalY);
+        var hyp = Math.sqrt(Math.pow(this.x - this.originalX, 2) + Math.pow(this.y - this.originalY, 2));
+        var offset = this.radius / hyp;
+        ctx.lineTo(this.x - this.radius,this.y - offset * (this.y - this.originalY));
+        ctx.stroke();
+    }
 }
 
+var circle: Circle = new Circle(document.body.clientWidth / 2, document.body.clientHeight * 3 / 4 + 25, 'rgb(205,133,63)');
+var circle2: Circle = new Circle(document.body.clientWidth / 2 + 10, document.body.clientHeight * 3 / 4 + 32, 'rgb(205,133,63)');
 /**
  * As edges "grow" as time passes, new nodes appear when the edge reaches a certain length. Find that length. 
  */
 function gameLoop(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    circle.draw();
+    circle2.draw();
+    circle.update(speed, 0);
+    circle2.update(speed, speed);
+    requestAnimationFrame(gameLoop);
     //drawCircle(document.body.clientWidth / 2, document.body.clientHeight * 3 / 4 + 40, 'rgb(205,133,63)', 15);
-    drawCircle(document.body.clientWidth / 2, document.body.clientHeight * 3 / 4 + 25, 'rgb(205,133,63)');
-    drawCircle(document.body.clientWidth / 2 + 10, document.body.clientHeight * 3 / 4 + 32, 'rgb(205,133,63)');
-    drawCircle(document.body.clientWidth / 2 - 10, document.body.clientHeight * 3 / 4 + 32, 'rgb(205,133,63)');
-    drawCircle(document.body.clientWidth / 2, document.body.clientHeight * 3 / 4 + 40, 'rgb(205,133,63)');
+    //drawCircle(document.body.clientWidth / 2, document.body.clientHeight * 3 / 4 + 25, 'rgb(205,133,63)');
+    //drawCircle(document.body.clientWidth / 2 + 10, document.body.clientHeight * 3 / 4 + 32, 'rgb(205,133,63)');
+    //drawCircle(document.body.clientWidth / 2 - 10, document.body.clientHeight * 3 / 4 + 32, 'rgb(205,133,63)');
+    //drawCircle(document.body.clientWidth / 2, document.body.clientHeight * 3 / 4 + 40, 'rgb(205,133,63)');
 }
 
 export class View{
@@ -44,6 +87,7 @@ export class View{
         this.setup(soil);
 
         tree = treeTemp;
+        gameLoop();
     }
     setup(canvas: HTMLCanvasElement): void{
         canvas.style.position = "absolute";
@@ -55,8 +99,5 @@ export class View{
     fill(): void{
         sctx.fillStyle = 'rgb(139,69,19)';
         sctx.fillRect(0,document.body.clientHeight * 3 / 4,document.body.clientWidth,document.body.clientHeight * 1 / 4);
-    
-        console.log("LOOP");
-        gameLoop();
     }
 }
